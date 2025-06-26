e74d62afd57bf2f7d15708758a3513f2fd046fd2
hrzentz <hrzentz@gmail.com>
2025-06-26 14:27:11 +0330

    accept and service connection added, not much configured

--- myCode/main.py
+++ myCode/main.py
@@ -1,5 +1,21 @@
 import socket
 import selectors
+import struct
+import types
+
+
+# create /etc/myhosts
+def create_myhosts():
+    path = "/etc/myhosts"
+    try:
+        with open(path, mode='x') as myhosts:
+            for line in ipv4s:
+                myhosts.write(line + '\n')
+    except FileExistsError:
+        print(f"File already exists: {path}")
+    except PermissionError:
+        print(f"Permission denied: You need to run this script as root to write to {path}")
+
 
 # Read IPv4s from /etc/hosts
 ipv4s = []
@@ -12,16 +28,39 @@ with open(path) as hosts:
             if len(parts) > 1 and parts[0].count('.') == 3:
                 ipv4s.append(line)
 
-# create /etc/myhosts
-path = "/etc/myhosts"
-try:
-    with open(path, mode='x') as myhosts:
-        for line in ipv4s:
-            myhosts.write(line + '\n')
-except FileExistsError:
-    print(f"File already exists: {path}")
-except PermissionError:
-    print(f"Permission denied: You need to run this script as root to write to {path}")
+
+# accept a new connection
+def accept_wrapper(sock):
+
+    # Because the listening socket was registered for the event selectors.EVENT_READ,
+    # it should be ready to read
+    conn, addr = sock.accept()  
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"") # !!!
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+
+# handle the events for existing connections
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            parse_message(recv_data)
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
 
 # find ipv4 address from /etc/myhosts
 def get_ipv4_address(domain_name):
@@ -37,9 +76,12 @@ def get_ipv4_address(domain_name):
 
 # parsing recieved message
 def parse_message(message):
-    query = {}
+    header = struct.unpack(">HHHHHH", message[:12])
+    dns_id, flags, quescount, anscount, nscount, arcount = header
 
-    queryid, flags, qdcount, ancount, nscount, arcount = s
+    print(f"ID : {dns_id}, Flags:{flags:016b}, Questions = {quescount}, Answers: {anscount}")
+    
+    
 
 # listening on port 5353
 sel = selectors.DefaultSelector()
@@ -56,9 +98,16 @@ print("listening for UDP on: " + host + ' ' + port)
 
 try:
     while True:
+        # wait for events
         events = sel.select(timeout=None)
         for key, mask in events:
-            if mask & selectors.EVENT_READ:
-                sock = key.fileobj
-                message, addr = sock.recvfrom(1024)
-                
+            if key.data is None: # check if this is a new connection request.
+                accept_wrapper(key.fileobj) # accept a new connection
+            else:
+                # handle the events for existing connections
+                service_connection(key, mask)
+
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
