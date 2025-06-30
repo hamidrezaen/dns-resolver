6d08f55538c602c0fb2dd5f07876c70dc5294a4a
hrzentz <hrzentz@gmail.com>
2025-06-30 18:34:02 +0330

    finished, correctness test done. concurrent test not.

--- myCode/main.py
+++ myCode/main.py
@@ -2,19 +2,6 @@ import socket
 import selectors
 import struct
 
-# create /etc/myhosts
-def create_myhosts():
-    path = "/etc/myhosts"
-    try:
-        with open(path, mode='x') as myhosts:
-            for line in ipv4s:
-                myhosts.write(line + '\n')
-    except FileExistsError:
-        print(f"File already exists: {path}")
-    except PermissionError:
-        print(f"Permission denied: You need to run this script as root to write to {path}")
-
-
 # Read IPv4s from /etc/myhosts
 ipv4s = []
 path = "/etc/myhosts"
@@ -35,9 +22,10 @@ def get_ipv4_address(domain_name):
             if not line.startswith('#') and line:
                 parts = line.split()
                 if parts[1] == domain_name:
-                    return parts[0].split('.')
+                    return [int(octet) for octet in parts[0].split('.')]
                 
         return None
+
 
 # parsing recieved message
 def parse_message(message):
@@ -50,20 +38,23 @@ def parse_message(message):
     offset = 12
 
     for _ in range(quescount):
-        domain_name, offset = decode_domain_name(message, offset)
+        start_i = offset
+        decoded_domain_name, offset = decode_domain_name(message, offset)
+        raw_domain_name = message[start_i:offset]
         qtype, qclass = struct.unpack(">HH", message[offset:offset+4])
         offset += 4
-        print(f"Query for: {domain_name}, Type: {qtype}, Class: {qclass}")
 
-    return{
+    print({
         "id" : dns_id,
         "flags" : flags,
         "questions" : quescount,
         "answers" : anscount,
-        "domain": domain_name,
+        "domain": decoded_domain_name,
         "qtype" : qtype,
         "qclass" : qclass
-    }
+    }, '\n'
+    )
+    return dns_id, flags, quescount, anscount, raw_domain_name, decoded_domain_name, qtype, qclass
 
 
 def decode_domain_name(message, offset):
@@ -78,10 +69,41 @@ def decode_domain_name(message, offset):
     return '.'.join(labels), offset
 
 # for now we just want to answer type A queries
-def create_typeA_answer(message):
+def create_answer(dns_id, raw_domain_name, decoded_domain_name, qtype, qclass, quescount):
+    ipv4_address = get_ipv4_address(decoded_domain_name)
     
-# create the myhosts file to read from
-create_myhosts()
+    answer = struct.pack('!H', dns_id)
+
+    is_type_class_valid = qclass==1 and qtype==1
+    does_exist = ipv4_address is not None
+
+    if not is_type_class_valid:
+        # the falg should be 1(QR)000 0(OPCODE)1(AA)0(TC)0(RD) 0(RA)000(Zero) 0100(RCODE)
+        answer += struct.pack('!HHHHH', 0x8404, quescount, 0, 0, 0)
+
+    elif does_exist:
+        # the falg should be 1(QR)000 0(OPCODE)1(AA)0(TC)0(RD) 0(RA)000(Zero) 0000(RCODE)
+        # so it is 1000 0100 0000 0000 = 0x8400
+        # then quescount=quscount, then anscount=1, nscount=0, arcount=0
+        answer += struct.pack('!HHHHH', 0x8400, quescount, 1, 0, 0)
+
+        answer += raw_domain_name
+        # add type and class
+        answer += struct.pack('!HH', qtype, qclass)
+        # ???
+        answer += b'\xc0\x0c'
+        # add type and class to the answer
+        answer += struct.pack('!HH', qtype, qclass)
+        # add TTL and datalength
+        answer += struct.pack('!LH', 0x0064, 4)
+
+        answer += struct.pack('!BBBB', *(ipv4_address))
+
+    else: # the domain name does not exist in the DNS
+        # the falg should be 1(QR)000 0(OPCODE)1(AA)0(TC)0(RD) 0(RA)000(Zero) 0011(RCODE)
+        answer += struct.pack('!HHHHH', 0x8403, quescount, 0, 0, 0)
+
+    return answer
 
 # listening on port 5353
 sel = selectors.DefaultSelector()
@@ -101,10 +123,12 @@ try:
         for key, mask in events:
             sock = key.fileobj
             if mask & selectors.EVENT_READ:
-                data, addr = sock.recvfrom(512)
+                message, addr = sock.recvfrom(512)
                 print(f"Recieved DNS query from {addr}")
-                parsed = parse_message(data)
-                print(parsed)
+                dns_id, flags, quescount, anscount, raw_domain_name, decoded_domain_name, qtype, qclass = parse_message(message)
+                response = create_answer(dns_id, raw_domain_name, decoded_domain_name, qtype, qclass, quescount)
+                sock.sendto(response, addr)
+
 except KeyboardInterrupt:
     print("Caught keyboard interrupt, exiting")
 finally:
